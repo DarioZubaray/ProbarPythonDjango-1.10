8d6c50c050488d15a5c00c6c1b45b2981d9df550
dario.zubaray <dzubaray@imagosur.com>
2019-07-05 08:42:47 -0300

    validaciones en el modelForm

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -5,6 +5,15 @@ class RegModelForm(forms.ModelForm):
     class Meta:
         model = Registrado
         fields = ["nombre", "email"]
+    
+    def clean_email(self):
+        print(self.cleaned_data)
+        emailForm = self.cleaned_data.get("email")
+        email_base, email_proveedor = emailForm.split("@")
+        email_dominio, email_extension = email_proveedor.split(".")
+        if not "edu" in email_extension:
+            raise forms.ValidationError("Por favor ingrese un correo edu")
+        return emailForm
 
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)

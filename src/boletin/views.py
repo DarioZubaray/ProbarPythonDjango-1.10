3c3487cf319af89c0c2bf88068d7e945e2938ce9
dario.zubaray <dzubaray@imagosur.com>
2019-07-05 09:07:33 -0300

    modelform en la vista

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import RegForm, RegModelForm
 from .models import Registrado
 
 # Create your views here.
@@ -10,21 +10,34 @@ def inicio(request):
     if request.user.is_authenticated():
         titulo = "Bienvenido %s" %(request.user)
 
-    form = RegForm(request.POST or None)
+    form = RegModelForm(request.POST or None)
+    context = {
+            "el_titulo": titulo,
+            "el_form": form
+        }
+
     print (dir(form)) #ver los metodos a utilizar del formulario
     if form.is_valid():
-        print(form.cleaned_data)
-        form_data = form.cleaned_data
-        emailForm = form_data.get("email")
-        nombreForm = form_data.get("nombre")
-        obj = Registrado.objects.create(email=emailForm, nombre=nombreForm)
+        instance = form.save(commit=False)
+        if not instance.nombre:
+            instance.nombre = "No declara nombre"
+        form.save()
+
+        context = {
+            "el_titulo": "Gracias %s!" %(instance.nombre)
+        }
+        print(instance)
+        print(instance.timestamp)
+
+        #print(form.cleaned_data)
+        #form_data = form.cleaned_data
+        #emailForm = form_data.get("email")
+        #nombreForm = form_data.get("nombre")
+        #obj = Registrado.objects.create(email=emailForm, nombre=nombreForm)
         
         #registrado = Registrado()
         #registrado.email = emailForm
         #registrado.save()
-    context = {
-        "el_titulo": titulo,
-        "el_form": form
-    }
+    
     return render(request, 'inicio.html', context)
     

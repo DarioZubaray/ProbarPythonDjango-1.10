e8365e5726bda5dfae3b1e0131d34ee3afeba583
dario.zubaray <dzubaray@imagosur.com>
2019-07-05 08:24:56 -0300

    guardando datos del formulario con el modelo

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 
 from .forms import RegForm
+from .models import Registrado
 
 # Create your views here.
 def inicio(request):
     form = RegForm(request.POST or None)
     print (dir(form)) #ver los metodos a utilizar del formulario
     if form.is_valid():
-        print form.cleaned_data
+        print(form.cleaned_data)
         form_data = form.cleaned_data
-        email = form_data.get("email")
-        #print form_data.get("edad")
+        emailForm = form_data.get("email")
+        nombreForm = form_data.get("nombre")
+        obj = Registrado.objects.create(email=emailForm, nombre=nombreForm)
+        
+        #registrado = Registrado()
+        #registrado.email = emailForm
+        #registrado.save()
     context = {
         "el_form": form
     }
     return render(request, 'inicio.html', context)
+    
